3c221a70a0d9cc21e1a616303342042bb105a53f
Renzo <i1821159@continental.edu.pe>
2020-06-24 13:07:47 -0500

    Img y Botones agregados del api de cada room

---
+++ app/src/main/java/com/example/hospedajeplacerandroid/RegisterDataClienteActivity.java
@@ -0,0 +1,14 @@
+package com.example.hospedajeplacerandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegisterDataClienteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_data_cliente);
+    }
+}
--- app/src/main/java/com/example/hospedajeplacerandroid/RoomDescriptionActivity.java
+++ app/src/main/java/com/example/hospedajeplacerandroid/RoomDescriptionActivity.java
@@ -4,9 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.NetworkImageView;
 import com.example.hospedajeplacerandroid.helpers.QueueUtils;
 import com.example.hospedajeplacerandroid.models.Room;
 
@@ -14,6 +19,7 @@ public class RoomDescriptionActivity extends AppCompatActivity {
     QueueUtils.QueueObject queue = null;
     int roomId;
     Room roomObject = new Room(0,"","","");
+    ImageLoader imageLoader;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,10 +33,24 @@ public class RoomDescriptionActivity extends AppCompatActivity {
         // CONSUMIR INFO DE HABITACION DE LA NUBE
         roomObject.id  = roomId;
         queue = QueueUtils.getInstance(this.getApplicationContext());
+        imageLoader = queue.getImageLoader(); //imagen
         Room.injectRoomFromCloud(queue, roomObject, this);
+
+        //LLAMAR A OTRO ACTIVITY DE LOS BOTONES
+        Button btnComprar = findViewById(R.id.btnComprar);
+        btnComprar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent o = new Intent(RoomDescriptionActivity.this, RegisterDataClienteActivity.class);
+                startActivity(o);
+            }
+        });
     }
     public void refresh(){
         TextView txtHabitacionName = findViewById(R.id.txtHabitacionName);
         txtHabitacionName.setText(roomObject.number_room);
+
+        NetworkImageView imgFoto = findViewById(R.id.imgFoto);
+        imgFoto.setImageUrl(roomObject.urlImage, imageLoader);
     }
 }
